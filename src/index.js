6183c7aa6969a73d5475e3552de574c00a03c807
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-03-04 20:54:21 +0300

    set up the files and basic structure

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function displayAnime (response) {
+    new Typewriter("#anime", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+function generateAnime(event) {
+
+    event.preventDefault();
+
+    let instructionInput = document.querySelector("#user-instructions");
+    
+    let apiKey = "e0ec6b874369tb90f96386aaf483o95a";
+    let context = "You are an anime expert and enjoy recommending anime. Your mission is to generate five best anime in a specific genre or year in basic html. Separate each line with a <br/> and number each line. Make sure to follow the user instructions. Sign the anime with  'SheCodes AI' inside a <strong> element.";
+    let prompt = `User instructions: List the best anime in ${instructionInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+
+}
+
